dd7c3359b9ada60782b0773b8047a5b1fa6f6f05
N.Tugsbieg <tugu0405@gmail.com>
2018-01-24 13:09:00 +0800

    fixed route

--- routes/web.php
+++ routes/web.php
@@ -24,7 +24,7 @@ Route::post('subscribe', 'AppController@subscribe');
 
 Auth::routes();
 
-Route::prefix('admin')->group(function () {
+Route::group(['prefix' => 'admin', 'middleware' => 'auth:admin'], function () {
     Route::get('/login', 'Auth\AdminLoginController@showLoginForm')->name('admin.login');
     Route::post('/login', 'Auth\AdminLoginController@login')->name('admin.login.submit');
     Route::post('/logout', 'Auth\AdminLoginController@logout')->name('admin.logout');
